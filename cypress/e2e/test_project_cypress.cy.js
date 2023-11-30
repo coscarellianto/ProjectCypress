6087e4394b01a83da103b5e2d51e648ba8a747b4
Antonella Coscarelli <coscarellianto@gmail.com>
2023-11-30 17:11:22 +0100

    - Add code for Aliexpress to open correctly - Add correct Aliexpress URL - Add code to accept cookies

--- cypress/e2e/test_project_cypress.cy.js
+++ cypress/e2e/test_project_cypress.cy.js
@@ -1,6 +1,14 @@
 /// <reference types="cypress" />
 
-it ('Iniciar Google', function(){
-    cy.visit('https://www.google.com')
-    cy.get('.spoKVd > :nth-child(1)').click()
+Cypress.on('uncaught:exception', (err, runnable) => {
+    if (err.message.includes('ResizeObserver')) {
+      // Evitar que Cypress falle por errores relacionados con ResizeObserver
+      return false;
+    }
+  });
+  
+it('Iniciar AliExpress', function(){
+    cy.visit('https://www.aliexpress.com')
+    cy.get(':nth-child(1) > #gdpr-new-container')
+    cy.get('.btn-accept').click()
 })
