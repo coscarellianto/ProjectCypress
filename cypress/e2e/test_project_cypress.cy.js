a0a7ec0d441f917dd8acaaf59aec7bc781e2f60a
Antonella Coscarelli <coscarellianto@gmail.com>
2023-12-07 17:48:53 +0100

    - Add cypress:run to run the test in the terminal - add validations locators paginationComponent - add step in the test

--- cypress/e2e/test_project_cypress.cy.js
+++ cypress/e2e/test_project_cypress.cy.js
@@ -11,32 +11,33 @@ let articles_page = new articlesPage()
 
 
 Cypress.on('uncaught:exception', (err, runnable) => {
-    if (err.message.includes('ResizeObserver')) {
-      // Prevent Cypress from crashing due to ResizeObserver-related errors
-      return false;
-    }
-  });
+  if (err.message.includes('ResizeObserver')) {
+    // Prevent Cypress from crashing due to ResizeObserver-related errors
+    return false;
+  }
+});
   
-  describe('Search element and check the stock', function(){
-    beforeEach(function () {
-        home_page.navigate();
-        home_page.checkCookiesManagement();
-        home_page.acceptManagementCookies();
-        home_page.closePopUpModal();
-    });
+describe('Search element and check the stock', () => {
+  beforeEach(function () {
+      home_page.navigate();
+      home_page.checkCookiesManagement();
+      home_page.acceptManagementCookies();
+      home_page.closePopUpModal();
+  });
 
-    //Search
-    it('Search Element', function(){
-      home_page.clickSearchBar();
-      home_page.typeSearchEnter('iphone');
-      home_page.clickSearchButton();
-      search_page.clickNextPage();
-      search_page.checkSecondArticle();
-      search_page.clickShoppingCard()
-      articles_page.getStockText().then((stockText) => {
-        cy.log(`Stock del artículo: ${stockText}`)
-        expect(parseInt(stockText)).to.be.greaterThan(0);
+  //Search
+  it('Search Element',() => {
+    home_page.clickSearchBar();
+    home_page.typeSearchEnter('iphone');
+    home_page.clickSearchButton();
+    search_page.checkComponentPagination();
+    search_page.clickNextPage();
+    search_page.checkSecondArticle();
+    search_page.clickShoppingCard()
+    articles_page.getStockText().then((stockText) => {
+      cy.log(`Stock del artículo: ${stockText}`)
+      expect(parseInt(stockText)).to.be.greaterThan(0);
     });
-    })
   })
+})
   
--- cypress/pages/search.js
+++ cypress/pages/search.js
@@ -1,9 +1,14 @@
 export class searchPage {
     //Locators
     constructor(){
+        this.paginationComponent = '.comet-pagination';
         this.paginationButton = '.comet-pagination-item-2 > a';
         this.secondCardList = '#card-list > :nth-child(2)';
         this.shoppingCard = ':nth-child(2) > .multi--outWrapper--SeJ8lrF > .multi--container--1UZxxHY > .multi--image--2bIiWPB > .multi--shopCart--darm7xs'
+    }
+
+    checkComponentPagination(){
+        cy.get(this.paginationComponent).should('exist')
     }
 
     clickNextPage(){
